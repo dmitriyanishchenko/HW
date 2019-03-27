71eeb7d17751b82bb1fbc4361e44990dd1eeef17
dmitriyanishchenko <48805227+dmitriyanishchenko@users.noreply.github.com>
2019-03-28 02:00:32 +0300

    add hw05

---
+++ task_5_1.py
@@ -0,0 +1,35 @@
+# Дана целочисленная матрица размером 5х5. Получить новую матрицу
+# путем деления всех элементов данной матрицы на ее наибольший по
+# модулю элемент
+
+from random import randint
+i = 0
+j = 0
+old_matrix = []
+for i in range(0, 5):
+    row = []
+    for j in range(0, 5):
+         row.append(randint(1, 50))
+    old_matrix.append(row)
+print(old_matrix)
+
+max_value = 0
+for i in range(0, 5):
+    for j in range(0, 5):
+        k = abs(old_matrix[i][j])
+        if k > max_value:
+            max_value = old_matrix[i][j]
+print(max_value)
+
+matrix = []
+el = 0
+for i in range(0, 5):
+    row = []
+    for j in range(0, 5):
+         el = round(old_matrix[i][j]/max_value, 2)
+         row.append(str(el))
+    matrix.append(row)
+print(matrix)
+
+for i in range(0, 5):
+    print(' '.join(matrix[i]))
