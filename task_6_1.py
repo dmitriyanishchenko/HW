4d7b204186d1fe6553447a0b1fe34df59f562675
dmitriyanishchenko <48805227+dmitriyanishchenko@users.noreply.github.com>
2019-04-02 17:25:52 +0300

    Add files task_6_1

---
+++ task_6_1.py
@@ -0,0 +1,31 @@
+# Написать программу, в которой вводятся два
+# операнда X и Y и знак операции (+ - * /)
+# вычисить результат в зависимости от знака.
+# предусмотреть реакции на неверный знак операции
+# а также на ввод 0 при делении
+i = 5
+sure_sign = ['+', '-', '*', '/', '0']
+while i > 0:
+
+    x = int(input('Enter number X -->'))
+    y = int(input('Enter number Y -->'))
+    sign = input('Enter + - * / or 0=stop')
+
+    if y == 0 and sign == '/':
+        print('На ноль делить нельзя!!!')
+        continue
+
+    if sign in sure_sign:
+        if sign == '0':
+            break
+        if sign == '+':
+            result = x + y
+        if sign == "-":
+            result = x - y
+        if sign == '*':
+            result = x * y
+        if sign == '/':
+            result = x / y
+        print(result)
+    else:
+        print('Неверный знак операции!!!')
