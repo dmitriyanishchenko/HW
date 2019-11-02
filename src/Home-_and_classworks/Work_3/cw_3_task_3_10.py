d6de54263fd49211f1ba0771f841f16025547f2a
Ditriy Anishchenko <1348010@gmail.com>
2019-11-02 08:06:24 +0300

    cw_3_task_3_10 start

---
+++ src/Home-_and_classworks/Work_3/cw_3_task_3_10.py
@@ -0,0 +1,5 @@
+# Получить на ввод количество рублей и копеек и вывести в правильной форме,
+# например, 3 рубля, 1 рубль 25 копеек, 3 копейки
+
+rubles = int(input('enter the number of rubles --->'))
+cents = int(input('enter the number of cents  --->'))
