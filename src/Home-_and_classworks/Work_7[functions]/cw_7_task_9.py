2e859445ad26b5fbf12ece25dd850c1ff5faf9a7
Ditriy Anishchenko <1348010@gmail.com>
2019-11-24 14:34:58 +0300

    cw_7_task_9 is complete

--- src/Home-_and_classworks/Work_7[functions]/cw_7_task_9.py
+++ src/Home-_and_classworks/Work_7[functions]/cw_7_task_9.py
@@ -1,5 +1,21 @@
 # Дан список чисел. Посчитать сколько раз встречается каждое число.
 # Подсказка: для хранения данных использовать словарь. Для проверки нахождения элемента в
 # словаре использовать метод get()
+from random import randint
 
-list_number = [1, 2,33,4, 5.,]
+
+def count_number(*args):
+    args = args[0]
+    res_dict = {}
+    for elem in args:
+        count = 0
+        for j in range(len(args)):
+            if elem == args[j]:
+                count += 1
+        res_dict[elem] = count
+    return res_dict
+
+
+list_number = [randint(0, 10) for i in range(10)]
+print(list_number)
+print(count_number(list_number))
