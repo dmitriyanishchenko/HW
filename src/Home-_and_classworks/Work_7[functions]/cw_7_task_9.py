ac94b5a5ed1cb0bb7f7092678af5df7cb66f7482
Ditriy Anishchenko <1348010@gmail.com>
2019-11-28 12:05:08 +0300

    cw_7_task_9 is changed

--- src/Home-_and_classworks/Work_7[functions]/cw_7_task_9.py
+++ src/Home-_and_classworks/Work_7[functions]/cw_7_task_9.py
@@ -5,7 +5,7 @@ from random import randint
 
 
 def count_number(*args):
-    args = args[0]
+    args = sorted(args[0])
     res_dict = {}
     for elem in args:
         count = 0
@@ -13,9 +13,10 @@ def count_number(*args):
             if elem == args[j]:
                 count += 1
         res_dict[elem] = count
-    return res_dict
+    for _, val in res_dict.items():
+        print(_, ' :: ', val)
 
 
-list_number = [randint(0, 10) for i in range(10)]
+list_number = [randint(0, 9) for i in range(50)]
 print(list_number)
-print(count_number(list_number))
+count_number(list_number)
