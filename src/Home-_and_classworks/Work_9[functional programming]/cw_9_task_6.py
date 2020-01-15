08db37e3ea2a448dd113aed45635e1e7d647fd3a
Ditriy Anishchenko <1348010@gmail.com>
2020-01-15 09:57:24 +0300

    cw_9 task 6 start

---
+++ src/Home-_and_classworks/Work_9[functional programming]/cw_9_task_6.py
@@ -0,0 +1,18 @@
+# Написать декоратор, который будет выводить время выполнения функции
+from datetime import datetime
+
+
+def my_decorator(func):
+   def do_some_staff():
+       # some action
+       func()
+       # some action
+   return do_some_staff
+
+
+@my_decorator
+def my_func():
+   pass
+
+my_new_func = my_decorator(my_func)
+my_new_func()
