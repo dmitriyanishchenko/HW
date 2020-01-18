febc34477d37eb6ad331dfba115b5030ee8eb65d
Ditriy Anishchenko <1348010@gmail.com>
2020-01-18 17:52:51 +0300

    Work_8 edited

--- src/Home-_and_classworks/Work_7_and_8[functions]/cw_8_task_8_2.py
+++ src/Home-_and_classworks/Work_7_and_8[functions]/cw_8_task_8_2.py
@@ -2,17 +2,14 @@
 
 
 def counter(*args):
-    print(args)
     count = {}
     for elem in args:
         if elem in count:
             count[elem] += 1
         else:
             count[elem] = 1
-    return count
+    for key, value in count.items():
+        print(f'Number {key} occurs {value} times')
 
 
 number = counter(1, 8, 7, 1, 6, 5, 2, 8)
-
-for key, value in number.items():
-    print(f'Number {key} occurs {value} times')
